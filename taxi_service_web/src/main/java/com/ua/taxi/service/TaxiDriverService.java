1570d6d17c38ea7951e851c0c1d26f9392c69bfb
Jura <jura_js@i.ua>
2015-05-15 12:55:21 +0300

    service package added

---
+++ taxi_service_web/src/main/java/com/ua/taxi/service/TaxiDriverService.java
@@ -0,0 +1,7 @@
+package com.ua.taxi.service;
+
+/**
+ * Created by Jura on 15.05.2015.
+ */
+public interface TaxiDriverService {
+}
---
+++ taxi_service_web/src/main/java/com/ua/taxi/service/TaxiDriverServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ua.taxi.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jura on 15.05.2015.
+ */
+@Service
+public class TaxiDriverServiceImpl implements TaxiDriverService {
+}
